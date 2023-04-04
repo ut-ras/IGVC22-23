d139c87ba8129ef9a73ff41e6782b857097fb195
Shivansh <106922533+shondle@users.noreply.github.com>
2023-04-03 19:26:07 -0500

    Restructure and update README

--- Old/Pothole Detection/newpothole.py
+++ Old/Pothole Detection/newpothole.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+
 img = cv.imread("./road_image.jpg")
 # img = cv.resize(img, (0,0), fx=0.5, fy=0.5)
 
@@ -8,7 +9,7 @@ print(ellipses[0][0][0])
 # Draw ellipse on original image
 for ellipse in ellipses:
     cv.ellipse(img, center=(int(ellipse[0][0]), int(ellipse[0][1])), axes=(int(ellipse[0][2]), int(ellipse[0][3])), angle=ellipse[0][4], startAngle=0, endAngle=360, color=(0, 255, 0), thickness=2)
-
+    print(f"The ellipse is {ellipse}")
 # Scale down the output image
 scale_percent = 30  # percent of original size
 width = int(img.shape[1] * scale_percent / 100)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # IGVC22-23
+
+
+## Current Code
+Most updated code is in the WIP Scripts folder. Find old code in the old folder. 
+
+## Dependencies
+Need OpenCV, python3, numpy, pyrealsense2
+Built for ROS Noetic
+
+FOR THE NEWER, FASTER, BETER ONE LABELLED newpothole.py- 
+pip install opencv-contrib-python
+- otherwise, you cannot use the functions used (that come from the 2017 paper you can find if you dig deeper)
+https://forum.opencv.org/t/a-fast-ellipse-detection-method-in-opencv-4-7-0/11586/2
