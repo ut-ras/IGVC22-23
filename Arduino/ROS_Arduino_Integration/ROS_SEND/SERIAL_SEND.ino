c7d470e60c3b7643b6999b091f1928e452645294
Tarunraj Govindarajan <63988484+tarunrajgm@users.noreply.github.com>
2024-02-04 18:36:22 -0600

    Update SERIAL_SEND.ino

--- Arduino/ROS_Arduino_Integration/ROS_SEND/SERIAL_SEND.ino
+++ Arduino/ROS_Arduino_Integration/ROS_SEND/SERIAL_SEND.ino
@@ -1,4 +1,14 @@
-#include <ros.h>
+/** 
+FUNCTIONALITY:
+
+THIS SCRIPT OPERATES ON ARDUINO #1 (connected to PC over serial and Arduino #2 over I2C)
+
+This script receives data from the robot PC over serial
+The received data can be interpreted as (left_motor_cmd, right_motor_cmd)
+The script transfers this info to Arduino #2 over I2C
+ **/
+
+
 #include <Wire.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/String.h>
@@ -7,21 +17,17 @@
 #define TA 8 // transmition address
 #define P_S 6 // Pin select low for left high for right
 
-ros::NodeHandle nh;
 
 void Send_To_Motors(int LEFT, int RIGHT);                 // sends ROS commands to motors thru slave device (arduino)
-void motor_cmd_str_callback(const std_msgs::String& msg); // processes the differential wheel velocity messages to values within (-127, 127)
 
-int ch3Value = 0;
-int ch2Value = 0;
+int ch3Value = 0;                                         // left motor value
+int ch2Value = 0;                                         // right motor value 
 int receivedInteger;
 
 
 void setup()
 {
-  nh.initNode();
-  nh.subscribe(sub);
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(P_S, OUTPUT);
   Wire.begin(); // join I2C bus (address optional for master)
 
@@ -31,7 +37,6 @@ void loop()
 {
   // continuously send current velocity commands to wheel-driving Arduino
   Send_To_Motors(ch3Value, ch2Value);
-  nh.spinOnce();
   delay(100);
 
   if (Serial.available() >= 8) {  // Adjust the number of bytes based on the data size
@@ -41,14 +46,15 @@ void loop()
     Serial.readBytes((char*)received_data, 8);
 
     // Unpack and print the received integers
-    Serial.print("Received Integers: ");
-    cmd_left = received_data[0]
+    // Serial.print("Received Integers: ");    // debug only
+    cmd_left = received_data[0]                      
     cmd_right = received_data[1]
 
-    ch3Value = 1.27 * cmd_left;
+    // scale values to [-127, 127]
+    ch3Value = 1.27 * cmd_left;     
     ch2Value = 1.27 * cmd_right;
 
-    Serial.print("Left velocity: "+" "+cmd_left+ "  "+"Right velocity: "+" "+cmd_right)
+    // Serial.print("Left velocity: "+" "+cmd_left+ "  "+"Right velocity: "+" "+cmd_right)   // debugging only
 
     Send_To_Motors(ch3Value, ch2Value);
   }
@@ -57,11 +63,15 @@ void loop()
 
 void Send_To_Motors(int LEFT, int RIGHT)
 {
+  // Map [-127, 127] to [0, 255], as required by Wire  (encode)
   int motor1_mapped_speed = map(LEFT, -127, 127, 0, 255);
   int motor2_mapped_speed = map(RIGHT, -127, 127, 0, 255);
+  
   Wire.beginTransmission(TA);
+  
   Wire.write(motor1_mapped_speed);
   Wire.write(motor2_mapped_speed);
   Wire.endTransmission();
+  
   delay(10);
 }
