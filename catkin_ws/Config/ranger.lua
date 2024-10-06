dd28d6fab398a28a4ede57ec421c225ff47cb72c
Tarunraj Govindarajan <63988484+tarunrajgm@users.noreply.github.com>
2024-10-06 12:10:44 -0500

    Update ranger.lua

--- catkin_ws/Config/ranger.lua
+++ catkin_ws/Config/ranger.lua
@@ -13,6 +13,8 @@
 -- limitations under the License.
 
 -- https://google-cartographer-ros.readthedocs.io/en/latest/configuration.html
+-- https://google-cartographer-ros.readthedocs.io/en/latest/algo_walkthrough.html
+-- https://google-cartographer-ros.readthedocs.io/en/latest/tuning.html
 
 include "map_builder.lua"
 include "trajectory_builder.lua"
@@ -21,16 +23,16 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",                       
-  tracking_frame = "base_link",             -- set to frame id of IMU (imu_link) if available
+  tracking_frame = "base_link",              -- set to frame id of IMU (imu_link) if available else base_link
   published_frame = "base_link",            -- should be "odom" if odometry frame available (e.g. from ekf or ukf), else base_link
   odom_frame = "odom",                      -- only used if provide_odom_frame is true
   provide_odom_frame = true,                -- publish odom_frame -> published_frame tf  (USE FALSE IF USING EKF / UKF)
-  publish_frame_projected_to_2d = true,     -- zero out roll, pitch, and z-offset 
+  publish_frame_projected_to_2d = false,     -- zero out roll, pitch, and z-offset 
   use_pose_extrapolator = true,
   use_odometry = false,                     -- subscribes to /odom (SET TO TRUE FOR EKF / UKF)
   use_nav_sat = false,                      -- GPS data, if published as sensor_msgs/NavSatFix on /fix topic
   use_landmarks = false,                    
-  num_laser_scans = 1,                      -- sensor_msgs/LaserScan on topic /scan
+  num_laser_scans = 0,                      -- sensor_msgs/LaserScan on topic /scan
   num_multi_echo_laser_scans = 0,            
   num_subdivisions_per_laser_scan = 10,     -- irrelevant (this if for multi-echo laser scans)
   num_point_clouds = 1,                     -- pointcloud2 on topic /points2
@@ -45,9 +47,47 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
-MAP_BUILDER.use_trajectory_builder_2d = true
--- WHAT DOES THIS DO??
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10 
+-- 3D SLAM 
+-- MUST USE IMU DATA IN 3D
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 160
+
+TRAJECTORY_BUILDER_3D.min_range = 0.1  -- meters
+TRAJECTORY_BUILDER_3D.max_range = 30.0
+
+TRAJECTORY_BUILDER_3D.use_online_correlative_scan_matching = true
+
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 5
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 4e2
+
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 160
+
+MAP_BUILDER.use_trajectory_builder_3d = true
+MAP_BUILDER.num_background_threads = 7
+POSE_GRAPH.optimization_problem.huber_scale = 5e2
+POSE_GRAPH.optimize_every_n_nodes = 320
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
+POSE_GRAPH.constraint_builder.min_score = 0.62
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+
+-- 2D SLAM
+-- TRAJECTORY_BUILDER_2D.min_range = 0.1  -- meters
+-- TRAJECTORY_BUILDER_2D.max_range = 30.0
+-- TRAJECTORY_BUILDER_2D.min_z = -2.0  
+-- TRAJECTORY_BUILDER_2D.max_z = 2.0
+
+-- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+-- TRAJECTORY_BUILDER_2D.use_imu_data = true         -- imu data must be on topic /imu and tf for imu_link must exist
+
+-- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 4e2
+-- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 4e3
+
+-- -- WHAT DOES THIS DO??
+-- TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1  -- was 10 
+
+-- POSE_GRAPH.constraint_builder.min_score = 0.65
+-- POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+-- POSE_GRAPH.optimize_every_n_nodes = 3 -- default is 3
 
 return options
 
